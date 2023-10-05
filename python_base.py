387763b98a0dac9b11da80800673d14a20566da7
Pralay <pralayghoshg9@gmail.com>
2023-10-05 15:09:56 +0530

    new base logic python

---
+++ python_base.py
@@ -0,0 +1,22 @@
+#1. Quotes
+x= '\'' #this shows single quote inside single quote -- \ this character is called escape character.
+print(x)
+#y= ''' #if we want to print it like this it will throw an error
+
+y= '\"' #Single quote inside double quote
+z= "\'"
+w = " ' "
+print(w)
+v='"'
+
+# New line
+n= 'Pralay\nGhosh'
+print(n)
+
+#tab line 
+n='Pralay\tGhosh'
+print(n)
+
+#backslash
+o= 'pralay\\ghosh'
+print(o)
